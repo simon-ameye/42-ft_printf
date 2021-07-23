b955bb61eade5c99fd3563cbc80bb197c81a5e63
simon-ameye <ameye.simon@gmail.com>
2021-07-23 17:47:36 +0200

    cleaned

--- test/test.sh
+++
@@ -1,6 +0,0 @@
-cd /Users/speedhacker/temp/printf
-make
-gcc -c -Wall -Wextra -Werror test/test.c -I libft/ -o test/test.o
-gcc -o test/test test/test.o libftprintf.a
-./test/test
-rm -rf test/test test/test.o
